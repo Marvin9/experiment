d9748658659c871ae924ea2ab1f5a64f5307d62c
Marvin9 <marvinduff97@gmail.com>
2020-04-05 19:33:46 +0530

    Do not slice last one

--- extract.js
+++ extract.js
@@ -4,8 +4,7 @@ const PULL_REGEX = /\/[0-9]+/i;
 
 if (PULL_REGEX.test(GITHUB_REF)) {
     const [PULL_REQUEST_NUMBER] = PULL_REGEX.exec(GITHUB_REF);
-    
-    process.stdout.write(PULL_REQUEST_NUMBER.slice(1, PULL_REQUEST_NUMBER.length-1));
+    process.stdout.write(PULL_REQUEST_NUMBER.slice(1, PULL_REQUEST_NUMBER.length));
 } else {
     process.stdout.write('FAILURE');
 }
