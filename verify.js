861e3314f10b32e1b766eb1cdef6caf88fedca11
Marvin9 <marvinduff97@gmail.com>
2020-04-05 19:27:12 +0530

    Fix

--- verify.js
+++ verify.js
@@ -1,8 +1,10 @@
-const [,,hook, user, comment] = process.argv;
+const [,,hook, user] = process.argv;
 
 if (hook && user && comment) {
     if (hook !== 'created' || user !== 'Marvin9') process.exit(1);
-    if (
-        comment.trim().toLowerCase() !== 'bottestthis'
-    ) process.exit(1);
+    const start = 4;
+    const end = process.argv.length;
+    let str = "";
+    for (let i = start; i < end; i++) str += process.argv[i].toLowerCase();
+    if (str !== 'bottestthis') process.exit(1);
 } else process.exit(1);
